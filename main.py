de29a86ceb0f3f1cefa06c7aa446047dd3d4ffa0
Bob <negenentwintig@hotmail.com>
2018-10-25 14:04:25 +0200

    dood eind

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import pandas as pd
+import numpy as np
 from scipy import stats
+import re
 
+def load_independencies(fn):
+    text = open(fn).read()
+    lists = [re.findall(r'Age|Sex|Diabetes|Thal|(?<=\()[a-z]+(?=\))', t) for t in text.split('\n')
+             if t != '']
+    print(lists)
+    lists = [[e.lower() for e in l] for l in lists]
+    tests = [(l[0], l[1], l[2:]) for l in lists if 'Diabetes' not in l]
+    return lists
+
+tests = load_independencies('origineel.txt')
 
 def test_independence(df, var1, var2, condition_vars=None):
     """
@@ -73,17 +85,136 @@ df.columns = ["age", "sex", "cp", "trestbps", "chol", "fbs", "restecg", "thalach
 
 linear_columns = ["age", "trestbps", "chol", "thalach", "oldpeak"]
 
-print(df[linear_columns].mean())
+print(df[linear_columns].median())
 
-df[linear_columns] = df[linear_columns] > df[linear_columns].mean()
+df[linear_columns] = df[linear_columns] > df[linear_columns].median()
 
 print(df)
 
-tests = [('cp', 'ca', ['num']), ('cp', 'exang', ['num']), ('trestbps', 'age', ['num', 'fbs', 'chol'])]
+tests = [
+# ('cp', 'exang', ['num', 'diabetes']),
+# ('cp', 'fbs', ['num', 'diabetes']),
+('cp', 'ca', ['num']),
+('cp', 'thalach', ['num']),
+('cp', 'restecg', ['num']),
+('cp', 'oldpeak', ['num']),
+# ('cp', 'chol', ['num', 'diabetes']),
+('cp', 'slope', ['num']),
+('cp', 'trestbps', ['num', 'fbs', 'chol']),
+# ('cp', 'trestbps', ['num', 'diabetes']),
+    # ('cp', 'age', ['num', 'diabetes']),
+    # ('cp', 'sex', ['num', 'diabetes']),
+('cp', 'thal', ['num']),
+# ('exang', 'fbs', ['num', 'diabetes']),
+('exang', 'ca', ['num']),
+('exang', 'thalach', ['num']),
+('exang', 'restecg', ['num']),
+('exang', 'oldpeak', ['num']),
+# ('exang', 'chol', ['num', 'diabetes']),
+('exang', 'slope', ['num']),
+('exang', 'trestbps', ['num', 'fbs', 'chol']),
+# ('exang', 'trestbps', ['num', 'diabetes']),
+    # ('exang', 'age', ['num', 'diabetes']),
+    # ('exang', 'sex', ['num', 'diabetes']),
+('exang', 'thal', ['num']),
+('fbs', 'ca', ['num']),
+('fbs', 'thalach', ['num']),
+('fbs', 'restecg', ['num']),
+('fbs', 'oldpeak', ['num']),
+('fbs', 'slope', ['num']),
+('fbs', 'thal', ['num']),
+('ca', 'thalach', ['num']),
+('ca', 'restecg', ['num']),
+('ca', 'oldpeak', ['num']),
+('ca', 'chol', ['num']),
+('ca', 'slope', ['num']),
+('ca', 'trestbps', ['num']),
+('ca', 'age', ['num']),
+    # ('ca', 'diabetes', ['num']),
+('ca', 'sex', ['num']),
+('ca', 'thal', ['num']),
+('thalach', 'restecg', ['num']),
+('thalach', 'oldpeak', ['num']),
+('thalach', 'chol', ['num']),
+('thalach', 'slope', ['num']),
+('thalach', 'trestbps', ['num']),
+('thalach', 'age', ['num']),
+    # ('thalach', 'diabetes', ['num']),
+('thalach', 'sex', ['num']),
+('thalach', 'thal', ['num']),
+('restecg', 'oldpeak', ['num']),
+('restecg', 'chol', ['num']),
+('restecg', 'slope', ['num']),
+('restecg', 'trestbps', ['num']),
+('restecg', 'age', ['num']),
+    # ('restecg', 'diabetes', ['num']),
+('restecg', 'sex', ['num']),
+('restecg', 'thal', ['num']),
+('oldpeak', 'chol', ['num']),
+('oldpeak', 'slope', ['num']),
+('oldpeak', 'trestbps', ['num']),
+('oldpeak', 'age', ['num']),
+    # ('oldpeak', 'diabetes', ['num']),
+('oldpeak', 'sex', ['num']),
+('oldpeak', 'thal', ['num']),
+('chol', 'slope', ['num']),
+    # ('chol', 'diabetes', ['fbs']),
+('chol', 'thal', ['num']),
+('slope', 'trestbps', ['num']),
+('slope', 'age', ['num']),
+    # ('slope', 'diabetes', ['num']),
+('slope', 'sex', ['num']),
+('slope', 'thal', ['num']),
+('trestbps', 'age', ['num', 'fbs', 'chol']),
+    # ('trestbps', 'diabetes', ['num', 'fbs', 'chol']),
+('trestbps', 'sex', ['num', 'fbs', 'chol']),
+('trestbps', 'thal', ['num']),
+    # ('age', 'diabetes', ['fbs']),
+('age', 'sex', []),
+('age', 'thal', ['num']),
+    # ('diabetes', 'sex', ['fbs']),
+    # ('diabetes', 'thal', ['num']),
+('sex', 'thal', ['num'])]
+
 
 # hoge p-waarde independent, lage p-waarde dependent
 
 for var1, var2, cond in tests:
     chi, p, dof = test_independence(df=df, var1=var1, var2=var2, condition_vars=cond)
-    print(p)
+    effect_size = np.sqrt(chi * chi / len(df) / dof)
+    if p > 0.05:
+        continue
     print(var1, 'and', var2, 'are', '' if p < 0.05 else 'not', 'dependent given', cond)
+    print('effect size =', effect_size)
+    print(p)
+
+# cp and oldpeak are  dependent given ['num']
+# effect size = 0.43626743727751777
+# 0.011028759792580445
+# exang and thalach are  dependent given ['num']
+# effect size = 0.41649772602405216
+# 0.006265886405995236
+# fbs and ca are  dependent given ['num']
+# effect size = 0.46312485408484716
+# 0.010511655849716361
+# ca and age are  dependent given ['num']
+# effect size = 0.4817054697309406
+# 0.007076949570464897
+# thalach and slope are  dependent given ['num']
+# effect size = 0.8276253063131302
+# 1.7250533146384583e-06
+# thalach and age are  dependent given ['num']
+# effect size = 0.45558106965700795
+# 0.0033009178368617054
+# thalach and thal are  dependent given ['num']
+# effect size = 0.35108995982893976
+# 0.04793932464994066
+# restecg and chol are  dependent given ['num']
+# effect size = 0.35296507673161354
+# 0.030478990520818372
+# oldpeak and slope are  dependent given ['num']
+# effect size = 1.151349936443901
+# 8.276490603975617e-10
+# sex and thal are  dependent given ['num']
+# effect size = 0.5862403256722045
+# 0.00041183761905361216
